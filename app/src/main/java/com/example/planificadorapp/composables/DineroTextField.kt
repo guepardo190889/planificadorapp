dd84112854f421df165c89556c9f0bae615c4407
Seth Karim Luis Martínez <seth.luis@banregio.com>
2024-09-09 18:02:48 -0600

    Se implementa nuevo text field para capturar dinero

--- app/src/main/java/com/example/planificadorapp/composables/DineroTextField.kt
+++ app/src/main/java/com/example/planificadorapp/composables/DineroTextField.kt
@@ -15,10 +15,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import com.example.planificadorapp.utilerias.FormatoMonto
 import java.math.BigDecimal
 import java.math.RoundingMode
 
+const val MAX_ENTEROS = 9
+
+/**
+ * Composable que representa un campo de entrada de texto para el saldo monetario
+ */
 @Composable
 fun MonetaryInputField(
     modifier: Modifier,
@@ -26,64 +32,8 @@ fun MonetaryInputField(
     isSaldoValido: Boolean,
     onSaldoChange: (BigDecimal) -> Unit
 ) {
-    val MAXIMO_CAPTURAS = 11
-
-    /**
-     * Calcula un texto de solo números considerando hasta 9 enteros y 2 decimales a partir de un BigDecimal
-     */
-    fun calcularCapturaUsuarioDesdeBigDecimal(monto: BigDecimal?): String {
-        var capturaUsuarioCalculada = ""
-
-        if (monto != null && monto > BigDecimal.ZERO) {
-            // Formatear el BigDecimal a un formato consistente con dos decimales
-            val montoFormateado = monto.setScale(2, RoundingMode.DOWN).toPlainString()
-            val partes = montoFormateado.split(".")
-            val enteros = partes[0] // Parte entera del número
-            val decimales = partes[1] // Parte decimal del número
-
-            // Concatenar enteros y decimales según las reglas de la tabla
-            capturaUsuarioCalculada = when {
-                // Si es un número de un solo dígito y su decimal es 0, mostrar solo el entero
-                enteros.length == 1 && decimales == "00" -> enteros
-                // Si es un número con un decimal diferente de 0, concatenar los enteros y el primer decimal
-                enteros.length == 1 && decimales != "00" -> enteros + decimales
-                // Para números con dos enteros (como 99.99)
-                enteros.length == 2 -> enteros + decimales
-                // Para números con tres enteros (como 999.99)
-                enteros.length == 3 -> enteros + decimales
-                // Para números con más de 3 dígitos en la parte entera
-                else -> enteros + decimales
-            }
-        }
-
-        return capturaUsuarioCalculada
-    }
-
-    var capturaUsuario by remember { mutableStateOf(calcularCapturaUsuarioDesdeBigDecimal(saldoInicial)) }
-    var monto by remember { mutableStateOf(BigDecimal.ZERO) }
-
-    /**
-     * Calcula un BigDecimal a partir de una cadena de texto de solo números considerando hasta 9 enteros y 2 decimales
-     */
-    fun calcularBigDecimal(capturaUsuario:String):BigDecimal{
-        var saldoBigDecimal: BigDecimal = BigDecimal.ZERO
-
-        if (capturaUsuario.isEmpty()) {
-            saldoBigDecimal = BigDecimal.ZERO
-        } else if (capturaUsuario.length == 1) {
-            saldoBigDecimal = BigDecimal(capturaUsuario)
-        } else if (capturaUsuario.length == 2) {
-            saldoBigDecimal = BigDecimal(capturaUsuario.substring(0, 1) + "." + capturaUsuario.substring(1, 2))
-        } else if (capturaUsuario.length == 3) {
-            saldoBigDecimal = BigDecimal(capturaUsuario.substring(0, 1) + "." + capturaUsuario.substring(1, 3))
-        } else if (capturaUsuario.length == 4) {
-            saldoBigDecimal = BigDecimal(capturaUsuario.substring(0, 2) + "." + capturaUsuario.substring(2, 4))
-        } else {
-            saldoBigDecimal = BigDecimal(capturaUsuario.substring(0, capturaUsuario.length - 2) + "." + capturaUsuario.substring(capturaUsuario.length - 2))
-        }
-
-        return saldoBigDecimal
-    }
+    var isCapturaEntera by remember { mutableStateOf(true) }
+    var monto by remember { mutableStateOf(saldoInicial ?: BigDecimal.ZERO) }
 
     OutlinedTextField(
         value = FormatoMonto.formatoSinSimbolo(monto),
@@ -95,42 +45,97 @@ fun MonetaryInputField(
         modifier = modifier
             .fillMaxWidth()
             .onKeyEvent { event ->
-                 when (event.nativeKeyEvent.keyCode) {
+                val montoString: String
+
+                val partes = monto
+                    .setScale(2, RoundingMode.DOWN)
+                    .toPlainString()
+                    .split(".")
+
+                val enteros = partes[0]
+                val decimales = partes[1]
+
+                when (event.nativeKeyEvent.keyCode) {
                     // Capturamos teclas numéricas
                     KeyEvent.KEYCODE_0, KeyEvent.KEYCODE_1, KeyEvent.KEYCODE_2, KeyEvent.KEYCODE_3, KeyEvent.KEYCODE_4,
-                    KeyEvent.KEYCODE_5, KeyEvent.KEYCODE_7, KeyEvent.KEYCODE_8, KeyEvent.KEYCODE_9 -> {
+                    KeyEvent.KEYCODE_5, KeyEvent.KEYCODE_6, KeyEvent.KEYCODE_7, KeyEvent.KEYCODE_8, KeyEvent.KEYCODE_9 -> {
                         val numeroPresionado = event.nativeKeyEvent.keyCode - KeyEvent.KEYCODE_0
 
-                        if(capturaUsuario.length < MAXIMO_CAPTURAS){
-                            capturaUsuario = numeroPresionado.toString() + capturaUsuario
-
-                            monto = calcularBigDecimal(capturaUsuario)
+                        if (isCapturaEntera) {
+                            if (enteros.length < MAX_ENTEROS) {
+                                montoString = enteros + numeroPresionado.toString()
+                                monto = BigDecimal(montoString)
+                            }
+                        } else {
+                            if (decimales.trimEnd('0').length < 2) {
+                                if (decimales
+                                        .trimEnd('0')
+                                        .isEmpty()
+                                ) {
+                                    montoString = "$enteros.$numeroPresionado"
+                                    monto = BigDecimal(montoString)
+                                } else if (decimales.trimEnd('0').length == 1) {
+                                    montoString =
+                                        enteros + "." + decimales.dropLast(1) + numeroPresionado.toString()
+                                    monto = BigDecimal(montoString)
+                                }
+                            }
                         }
 
                         onSaldoChange(monto)
 
                         true
                     }
+
+                    KeyEvent.KEYCODE_PERIOD -> {
+                        isCapturaEntera = false
+
+                        true
+                    }
+
                     KeyEvent.KEYCODE_DEL -> {
-                        if(capturaUsuario.isNotEmpty()){
-                            if(capturaUsuario.length == 1) {
-                                capturaUsuario = ""
+                        if (isCapturaEntera) {
+                            montoString = enteros.dropLast(1)
+                        } else {
+                            if (decimales
+                                    .trimEnd('0')
+                                    .isEmpty()
+                            ) {
+                                isCapturaEntera = true
+                                montoString = enteros.dropLast(1)
+                            } else if (decimales.trimEnd('0').length == 1) {
+                                montoString = enteros
+                            } else {
+                                montoString = enteros + "." + decimales.dropLast(1)
                             }
-                            else {
-                                capturaUsuario = capturaUsuario.substring(1, capturaUsuario.length)
-                            }
+                        }
 
-                            monto = calcularBigDecimal(capturaUsuario)
+                        monto = if (montoString.isEmpty()) {
+                            BigDecimal.ZERO
+                        } else {
+                            BigDecimal(montoString)
                         }
 
                         onSaldoChange(monto)
 
                         true
                     }
+
                     else -> true
                 }
             },
         keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+        supportingText = {
+            if (!isSaldoValido) {
+                Text(
+                    text = "El saldo es requerido",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End
+                )
+            }
+        },
         colors = OutlinedTextFieldDefaults.colors()
     )
 }
