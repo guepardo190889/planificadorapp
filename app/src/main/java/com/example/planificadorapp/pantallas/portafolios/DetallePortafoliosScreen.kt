98370e0524d9540d66a5bbb42dba77bf2b536bb2
Seth Karim Luis Martínez <seth.luis@banregio.com>
2024-09-14 12:48:07 -0600

    Versión refactorizada y optimizada

--- app/src/main/java/com/example/planificadorapp/pantallas/portafolios/DetallePortafoliosScreen.kt
+++ app/src/main/java/com/example/planificadorapp/pantallas/portafolios/DetallePortafoliosScreen.kt
@@ -1,15 +1,9 @@
 package com.example.planificadorapp.pantallas.portafolios
 
-import android.util.Log
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -29,190 +23,125 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.planificadorapp.composables.TextoConEtiqueta
 import com.example.planificadorapp.composables.graficos.GraficaPastelCanvas
+import com.example.planificadorapp.modelos.portafolios.busqueda.PortafolioBuscarComposicionResponseModel
+import com.example.planificadorapp.modelos.portafolios.busqueda.PortafolioBuscarCuentaResponseModel
 import com.example.planificadorapp.modelos.portafolios.busqueda.PortafolioBuscarResponseModel
 import com.example.planificadorapp.modelos.portafolios.graficos.DistribucionPortafolioGraficoModel
 import com.example.planificadorapp.repositorios.PortafoliosRepository
 import com.example.planificadorapp.utilerias.FormatoMonto
-import java.math.BigDecimal
 
 /**
- * Composable que representa la pantalla de detalle de un portafolio
+ * Composable que representa la pantalla de detalles de un portafolio
  */
 @Composable
 fun DetallePortafoliosScreen(
     modifier: Modifier = Modifier, navController: NavController, idPortafolio: Long
 ) {
     val portafoliosRepository = PortafoliosRepository()
-
     var portafolio by remember { mutableStateOf<PortafolioBuscarResponseModel?>(null) }
     var datosGrafico by remember { mutableStateOf<DistribucionPortafolioGraficoModel?>(null) }
 
     LaunchedEffect(idPortafolio) {
         portafoliosRepository.buscarPortafolioPorId(idPortafolio) { portafolioEncontrado ->
-            if (portafolioEncontrado != null) {
-                portafolio = portafolioEncontrado
-                Log.i("DetallePortafoliosScreen", "Portafolio encontrado: $portafolio")
-
-                portafoliosRepository.buscarDatosGraficoDistribucion(idPortafolio) {
-                    datosGrafico = it
-                    Log.i("DetallePortafoliosScreen", "Datos del portafolio: $it")
-                }
+            portafolio = portafolioEncontrado
+            portafoliosRepository.buscarDatosGraficoDistribucion(idPortafolio) {
+                datosGrafico = it
             }
         }
     }
 
-    Scaffold(modifier = modifier.fillMaxWidth(), floatingActionButton = {
+    Scaffold(floatingActionButton = {
         FloatingActionButton(
-            modifier = Modifier.padding(16.dp),
+            onClick = { /* Edit action */ },
             containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary,
-            onClick = {
-                //navController.navigate("portafolios/editar/${idPortafolio}")
-            },
+            contentColor = MaterialTheme.colorScheme.onPrimary
         ) {
             Icon(Icons.Default.Edit, contentDescription = "Actualizar Portafolio")
         }
-    }, content = { paddingValues ->
+    }) { paddingValues ->
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .padding(paddingValues)
-                .fillMaxWidth()
                 .verticalScroll(rememberScrollState())
-                .border(2.dp, MaterialTheme.colorScheme.primary),
         ) {
-            portafolio?.let {
+            portafolio?.let { portafolio ->
+                // Card for general info
                 Card(
-                    modifier = Modifier.fillMaxWidth(), colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surface,
-                        contentColor = MaterialTheme.colorScheme.onSurface
-                    )
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
                 ) {
-                    Column(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                    ) {
-                        portafolio?.let {
-                            TextoConEtiqueta(
-                                etiqueta = "Nombre: ",
-                                texto = it.nombre ?: "",
-                                styleLabel = "large",
-                                styleBody = "medium"
-                            )
-                            TextoConEtiqueta(
-                                etiqueta = "Saldo: ",
-                                texto = FormatoMonto.formato(it.saldo),
-                                styleLabel = "large",
-                                styleBody = "medium"
-                            )
-                            TextoConEtiqueta(
-                                etiqueta = "Descripción: ",
-                                texto = it.descripcion,
-                                styleLabel = "large",
-                                styleBody = "medium"
-                            )
-                        }
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        TextoConEtiqueta("Nombre", portafolio.nombre ?: "", "large", "medium")
+                        TextoConEtiqueta(
+                            "Saldo", FormatoMonto.formato(portafolio.saldo), "large", "medium"
+                        )
+                        TextoConEtiqueta("Descripción", portafolio.descripcion, "large", "medium")
                     }
                 }
 
-                Column(modifier = modifier.border(2.dp, Color.Yellow)) {
-                    Text(
-                        modifier = modifier.padding(16.dp).border(2.dp, Color.Black), text = "Activos: "
-                    )
-                    //Composiciones
-                    LazyColumn(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .height(200.dp)
-                            .border(2.dp, MaterialTheme.colorScheme.primary)
-                    ) {
-                        items(it.composiciones) { composicion ->
-                            ListItem(modifier = modifier,
-                                //.border(2.dp, MaterialTheme.colorScheme.error),
-                                headlineContent = {
-                                    Text(
-                                        text = composicion.nombreActivo ?: "",
-                                        color = MaterialTheme.colorScheme.onSurface
-                                    )
-                                }, trailingContent = {
-                                    Text(
-                                        text = composicion.porcentaje.toString() + " %",
-                                        color = MaterialTheme.colorScheme.onSurface
-                                    )
-                                })
-
-                            Column(
-                                modifier = modifier.padding(16.dp)
-                            ) {
-                                if (composicion.cuentas.isNotEmpty()) {
-                                    Text(
-                                        modifier = modifier,//.border(2.dp, Color.Green),
-                                        text = "Cuentas asociadas: ",
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onSurface,
-                                    )
-                                    composicion.cuentas.forEach { cuenta ->
-                                        Row(modifier = modifier.fillMaxWidth()) {
-                                            Text(
-                                                modifier = modifier.weight(1f),
-                                                //.border(2.dp, Color.Blue),
-                                                text = cuenta.nombre ?: "",
-                                                textAlign = TextAlign.Left,
-                                                style = MaterialTheme.typography.bodySmall,
-                                                color = MaterialTheme.colorScheme.onSurface,
-                                            )
-                                            Text(
-                                                modifier = modifier.weight(1f),
-                                                //.border(2.dp, Color.Cyan),
-                                                text = FormatoMonto.formato(cuenta.saldo),
-                                                textAlign = TextAlign.End,
-                                                style = MaterialTheme.typography.bodySmall,
-                                                color = MaterialTheme.colorScheme.onSurface,
-                                            )
-                                        }
-                                    }
-                                } else {
-                                    Text(
-                                        text = "No hay cuentas asociadas",
-                                        style = MaterialTheme.typography.bodySmall,
-                                        color = MaterialTheme.colorScheme.onSurface
-                                    )
-                                }
-                            }
-
-                            HorizontalDivider(color = MaterialTheme.colorScheme.outline)
-                        }
-                    }
-                }
-
+                // Activos y composiciones
+                ComposicionesList(composiciones = portafolio.composiciones)
             }
 
-            if (datosGrafico != null) {
-                if (datosGrafico!!.composicionesPortafolio.isNotEmpty()) {
-                    val datos = mutableListOf<Pair<String, Float>>()
-                    for (composicion in datosGrafico!!.composicionesPortafolio) {
-                        datos.add(
-                            Pair(
-                                composicion.nombreActivo, composicion.saldoTotalCuentas.toFloat()
-                            )
-                        )
+            // Gráfico de pastel
+            datosGrafico?.let { grafico ->
+                if (grafico.composicionesPortafolio.isNotEmpty()) {
+                    val datos = grafico.composicionesPortafolio.map {
+                        it.nombreActivo to it.saldoTotalCuentas.toFloat()
                     }
 
-                    Log.i("DetallePortafoliosScreen", "Datos del portafolio: $datos")
-
                     GraficaPastelCanvas(
-                        modifier = modifier, titulo = "Distribución", datos
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        titulo = "Distribución", datos = datos
                     )
                 }
             }
         }
-    })
+    }
+}
+
+@Composable
+fun ComposicionesList(composiciones: List<PortafolioBuscarComposicionResponseModel>) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        composiciones.forEach { composicion ->
+            ListItem(
+                headlineContent = { Text(text = composicion.nombreActivo ?: "") },
+                trailingContent = { Text(text = "${composicion.porcentaje} %") }
+            )
+            CuentasList(cuentas = composicion.cuentas)
+            HorizontalDivider(color = MaterialTheme.colorScheme.outline)
+        }
+    }
+}
+
+@Composable
+fun CuentasList(cuentas: List<PortafolioBuscarCuentaResponseModel>) {
+    if (cuentas.isNotEmpty()) {
+        cuentas.forEach { cuenta ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = cuenta.nombre ?: "", modifier = Modifier.weight(1f))
+                Text(
+                    text = FormatoMonto.formato(cuenta.saldo),
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.End
+                )
+            }
+        }
+    } else {
+        Text(text = "No hay cuentas asociadas", style = MaterialTheme.typography.bodySmall)
+    }
 }
