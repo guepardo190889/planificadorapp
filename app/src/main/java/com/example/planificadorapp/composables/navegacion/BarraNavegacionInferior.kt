c9b07b1c3e8956253d58e0a574d9e679bf2bcc4d
Seth Karim Luis Martínez <seth.luis@banregio.com>
2024-09-20 23:27:07 -0600

    Se cambia implementación de la barra inferior para usar un NavigationBar

--- app/src/main/java/com/example/planificadorapp/composables/navegacion/BarraNavegacionInferior.kt
+++ app/src/main/java/com/example/planificadorapp/composables/navegacion/BarraNavegacionInferior.kt
@@ -1,7 +1,5 @@
 package com.example.planificadorapp.composables.navegacion
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -9,16 +7,14 @@ import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
-import androidx.compose.material3.TooltipBox
-import androidx.compose.material3.TooltipDefaults
-import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 /**
  * Composable que muestra la barra inferior de navegación
@@ -32,72 +28,56 @@ fun BarraNavegacionInferior(
     onSiguienteClick: (() -> Unit)? = null,
     onTransaccionClick: (() -> Unit)? = null
 ) {
-    BottomAppBar(modifier = modifier.fillMaxWidth(),
+    BottomAppBar(
+        modifier = modifier.fillMaxWidth(),
         containerColor = MaterialTheme.colorScheme.surface,
-        contentColor = MaterialTheme.colorScheme.onSurface,
-        content = {
-            Row(
-                modifier = Modifier.fillMaxWidth(), horizontalArrangement = when {
-                    onAtrasClick != null && onSiguienteClick != null -> Arrangement.SpaceBetween
-                    onAtrasClick != null && onTransaccionClick != null -> Arrangement.SpaceBetween
-                    onAtrasClick != null -> Arrangement.Start
-                    onSiguienteClick != null -> Arrangement.End
-                    else -> Arrangement.Center
-                }
-            ) {
-                if (onAtrasClick != null) {
-                    TooltipBox(
-                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
-                        tooltip = { PlainTooltip { Text("Atrás") } },
-                        state = rememberTooltipState()
-                    ) {
-                        FloatingActionButton(
-                            onClick = onAtrasClick,
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = MaterialTheme.colorScheme.onPrimary
-                        ) {
-                            Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Atrás")
-                        }
-                    }
-                }
-
-                if (onSiguienteClick != null) {
-                    TooltipBox(
-                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
-                        tooltip = { PlainTooltip { Text("Siguiente") } },
-                        state = rememberTooltipState()
-                    ) {
-                        FloatingActionButton(
-                            onClick = onSiguienteClick,
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = MaterialTheme.colorScheme.onPrimary
-                        ) {
-                            Icon(
-                                Icons.AutoMirrored.Default.ArrowForward,
-                                contentDescription = "Siguiente"
-                            )
-                        }
-                    }
-                }
-
-                if (onTransaccionClick != null) {
-                    TooltipBox(
-                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
-                        tooltip = { PlainTooltip { Text(text = if (isTransaccionGuardar == true) "Guardar" else "Actualizar") } },
-                        state = rememberTooltipState()
-                    ) {
-                        FloatingActionButton(
-                            onClick = onTransaccionClick,
-                            containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = MaterialTheme.colorScheme.onPrimary
-                        ) {
-                            Icon(
-                                Icons.Default.Done,
-                                contentDescription = if (isTransaccionGuardar == true) "Guardar" else "Actualizar"
-                            )
-                        }
-                    }
-                }
+        contentColor = MaterialTheme.colorScheme.onSurface
+    ) {
+        NavigationBar(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ) {
+            if (onAtrasClick != null) {
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = "Atrás"
+                        )
+                    },
+                    label = { Text("Atrás") },
+                    selected = false,
+                    onClick = { onAtrasClick() }
+                )
             }
-        })
+
+            if (onSiguienteClick != null) {
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            Icons.AutoMirrored.Default.ArrowForward,
+                            contentDescription = "Adelante"
+                        )
+                    },
+                    label = { Text("Adelante") },
+                    selected = false,
+                    onClick = { onSiguienteClick() }
+                )
+            }
+
+            if (onTransaccionClick != null) {
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            Icons.Default.Done,
+                            contentDescription = if (isTransaccionGuardar == true) "Guardar" else "Actualizar"
+                        )
+                    },
+                    label = { Text(if (isTransaccionGuardar == true) "Guardar" else "Actualizar") },
+                    selected = false,
+                    onClick = { onTransaccionClick() }
+                )
+            }
+        }
+    }
 }
