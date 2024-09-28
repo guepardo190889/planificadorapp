f6d984b1f37c8f8be78eb3031831785ce2a96b94
Seth Karim Luis Martínez <seth.luis@banregio.com>
2024-09-27 23:53:55 -0600

    Se quita código no usado

--- app/src/main/java/com/example/planificadorapp/modelos/portafolios/graficos/DistribucionPortafolioGraficoModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.planificadorapp.modelos.portafolios.graficos
-
-/**
- * Modelo de datos para representar una distribución de un portafolio en un gráfico
- */
-data class DistribucionPortafolioGraficoModel(
-    val nombrePortafolio: String,
-    val composicionesPortafolio: List<ComposicionGraficoModel>
-)
--- app/src/main/java/com/example/planificadorapp/servicios/PortafoliosService.kt
+++ app/src/main/java/com/example/planificadorapp/servicios/PortafoliosService.kt
@@ -3,7 +3,6 @@ package com.example.planificadorapp.servicios
 import com.example.planificadorapp.modelos.portafolios.PortafolioGuardarRequestModel
 import com.example.planificadorapp.modelos.portafolios.PortafolioModel
 import com.example.planificadorapp.modelos.portafolios.busqueda.PortafolioBuscarResponseModel
-import com.example.planificadorapp.modelos.portafolios.graficos.DistribucionPortafolioGraficoModel
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -42,10 +41,4 @@ interface PortafoliosService {
      */
     @GET("api/portafolios")
     fun buscarPortafolios(): Call<List<PortafolioModel>>
-
-    /**
-     * Busca en el servidor los datos necesarios para generar un gráfico de distribución y devuelve un DistribucionPortafolioGraficoModel
-     */
-    @GET("api/portafolios/{id}/grafico-distribucion")
-    fun buscarDatosGraficoDistribucion(@Path("id") id: Long): Call<DistribucionPortafolioGraficoModel>
 }
